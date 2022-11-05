1c900eaf7b2040742ac8d8c3b7c98b232c494c53
Daniel Nüst <daniel.nuest@tu-dresden.de>
2022-11-05 01:16:15 +0100

    add slides for introduction video
    https://youtu.be/_nMzFhYro_U

---
+++ 2022-11_GRN-Video/macros.js
@@ -0,0 +1,25 @@
+remark.macros.scale = function (percentage) {
+  var url = this;
+  return '<img src="' + url + '" style="width: ' + percentage + '" />';
+};
+
+remark.macros.inline_highlight = function () {
+  var word = this;
+  return '<span class="inline_highlight">' + word + '</span>';
+};
+remark.macros.highlight_inline = function () {
+  var word = this;
+  return '<span class="inline_highlight">' + word + '</span>';
+};
+remark.macros.scale_text = function (percentage) {
+  var words = this;
+  return '<div style="font-size: ' + percentage + '%">' + words + '</div>';
+};
+remark.macros.scale_text_inline = function (percentage) {
+  var words = this;
+  return '<span style="font-size: ' + percentage + '%">' + words + '</span>';
+};
+remark.macros.credit = function (percentage) {
+  var words = this;
+  return '<span class="credit">' + words + '</span>';
+};
